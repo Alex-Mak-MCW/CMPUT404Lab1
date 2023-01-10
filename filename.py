2657a4f5ddb5811b4cf75687e3218341b1ec1bb9
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2023-01-09 18:51:20 -0700

    Second commit

--- filename.py
+++ filename.py
@@ -3,3 +3,4 @@ import requests
 print(requests.__version__)
 
 print(requests.get('http://google.com/').text)
+print(requests.get('https://raw.githubusercontent.com/Alex-Mak-MCW/CMPUT404Lab1/master/filename.py').text)
